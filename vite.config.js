f08756a357c41275629062d6b83ef0329848d622
deproger <vl.ebel.tm@gmail.com>
2024-10-17 00:21:42 +0700

    changed vite

--- vite.config.js
+++ vite.config.js
@@ -4,31 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Позволяет использовать любой хост
-    port: 5713, // Основной порт для разработки, можно изменить при необходимости
-    hmr: {
-      protocol: "ws", // Протокол для HMR (горячая перезагрузка модулей)
-    },
-    proxy: {
-      // Настройка для subdomain: admin.localhost
-      "/admin": {
-        target: "https://admin.24t-taxi.ru",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/admin/, ""),
-      },
-      // Настройка для subdomain: client.localhost
-      "/client": {
-        target: "https://client.24t-taxi.ru",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/client/, ""),
-      },
-      // Настройка для subdomain: driver.localhost
-      "/driver": {
-        target: "https://driver.24t-taxi.ru",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/driver/, ""),
-      },
-    },
-  },
 });
